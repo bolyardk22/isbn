b9addea056dc7f2ea26bce8592368c83892d6eab
bolyardk22 <collinsk22@mycu.concord.edu>
2017-08-29 16:08:27 -0400

    checks last digit for ten digits

--- isbn.rb
+++ isbn.rb
@@ -1,4 +1,4 @@
-def check_isbn (sampleISBN)
+def checkISBNlength (sampleISBN)
 
 	sampleISBN = sampleISBN.delete(' ')
 
@@ -12,3 +12,52 @@ def check_isbn (sampleISBN)
 		false
 	end
 end
+
+
+def cleanISBN (sampleISBN2)
+
+	sampleISBN2 = sampleISBN2.delete(' ')
+
+	sampleISBN2 = sampleISBN2.delete('-')
+
+	result_sampleISBN2 = sampleISBN2
+end
+
+
+def check_if_last_num_passes(cleanedISBN)
+
+		math_array = []
+
+	if cleanedISBN.length == 10
+
+		arrayISBN = cleanedISBN.split('')
+
+		last_positionISBN = arrayISBN.pop
+
+		arrayISBN.each.with_index do |var, index|
+			mult = var.to_f * (index.to_f + 1)
+			math_array << mult
+		end
+	end
+
+	math_array_sum = math_array.inject(0, :+)
+
+	math_array_mod = math_array_sum % 11
+
+	if math_array_mod <= 9
+		math_array_mod
+	elsif math_array_mod ==10
+		math_array_mod = "X"
+	else
+		false
+	end
+
+	if math_array_mod == last_positionISBN
+		true
+	else
+		false
+	end
+
+end
+
+p check_if_last_num_passes("123456789X")
--- test_isbn.rb
+++ test_isbn.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require_relative "isbn.rb"
+
 class TestISBN_Machine < Minitest::Test
 
 	def test_1_equals_1
@@ -7,14 +8,31 @@ class TestISBN_Machine < Minitest::Test
 	end
 
 	def test_length_equals_six_fails
-		assert_equal(false,check_isbn("123456"))
+		assert_equal(false,checkISBNlength("123456"))
 	end
 
 	def test_length_equals_ten_passes
-		assert_equal(true,check_isbn("1234567891"))
+		assert_equal(true,checkISBNlength("1234567891"))
 	end
 
-	def test_removes_dashes
-		assert_equal(true,check_isbn("123456789-1"))
+	def test_removes_dashes_length
+		assert_equal(true,checkISBNlength("123456789-1"))
 	end
+
+	def test_removes_spaces_length
+		assert_equal(true,checkISBNlength("123 123 123 4"))
+	end
+
+	def test_removes_dashes_equals
+		assert_equal("1234567891",cleanISBN("123 456 789-1"))
+	end
+
+	def test_checks_last_digit_for_ten_true
+		assert_equal(true,check_if_last_num_passes("123456789X"))
+	end
+
+	def test_checks_last_digit_for_ten_false
+		assert_equal(false,check_if_last_num_passes("1234567894"))
+	end
+
 end
